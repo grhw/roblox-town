261738ac0fa94a8f0d45f1210ac0247d7a7b0516
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-03-18 03:01:13 +0800

    i

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from markupsafe import escape
 from flask import Response
 from flask import Flask
+from time import sleep
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    requests.get("https://roblox-town-gen.onrender.com/legacy/")
+    
     return open("./html/index.html","r").read()
 
 @app.route('/legacy/')
 def legacy():
     return open("./html/legacy/index.html","r").read()
+
+@app.route('/cdn/<test>/<ex>')
+def get(test,ex):
+    return open(f"./html/{test}.{ex}","r").read()
+
+@app.route('/cdn/legacy/<test>/<ex>')
+def getlegacy(test,ex):
+    return open(f"./html/legacy/{test}.{ex}","r").read()
+
+
+while True:
+    print(requests.get("https://roblox-town-gen.onrender.com/legacy/").status_code)
+    sleep(5)
