dd3a7ffd490af5641961a264edebc7768641dc9a
achoolucgust <76622722+achoolucgust@users.noreply.github.com>
2023-03-19 16:35:24 +0800

    yup

--- html/script.js
+++ html/script.js
@@ -1,113 +1,154 @@
-var Attachments = `ACOG Sight
-Angled Grip
-Bipod
+var Attachments = `Suppressor
+Heavy Suppressor
+Green Laser
 CL6X Sight
 Compensator
-Coyote Sight
+Hunting Scope
 EOTech Sight
 Ergo Grip
 Extended Stock
 Flash Hider
-Flashlight
 Folding Grip
-Green Laser
-Heavy Suppressor
-Hunting Scope
+ACOG Sight
+Flashlight
+Angled Grip
+Coyote Sight
 JGM4 Sight
+PSO-1 Scope
 Kobra Sight
 M145 Sight
-PSO-1 Scope
+Wide Choke
 Red Laser
-Suppressor
-Wide Choke`.split("\n")
-var Armor = `COM
-FBI
-GRU
-HCS
-RGF
-Riot
+Bipod`.split("\n")
+var Armor = `--: Armor :--
+
 Robes
-Robes2
-SAS
-Slav
-SWAT
 Tenor
+Robes2
+HCS
+COM
+SWAT
+FBI
+RGF
+Slav
+Riot
 VDV
-Rusky (GAMEPASS)`.split("\n")
-var Weapons = `AK-47
-AS VAL
-AUG A3
-CheyTac M200
-CZ-75
-Desert Eagle
-Dragunov SVD
-FAMAS F1
-Five SeveN
-G36C
-Glock-18
-HK416
-JNG-90
+SAS
+GRU
+
+--: Gamepass :--
+
+Rusky`.split("\n")
+var Weapons = `--: Guns :--
+
+Tumor Growth
 L85A2
-L96A1
-M16A3
 M1911
+Steyr Elite
 M4
-M9
-MAC-10
-Magnum Revolver
-Makarov
-MG4
-MK-11
 MP443
-MP5K
 MP7
-P226
-P90
-QBU-88
-QBZ-95
+UMP-40
+MG4
 Remington 870
 RFB
-RPK-74M
 Saddlegun
-SKS
+Glock-18
+Desert Eagle
+AK-47
+M16A3
+CZ-75
+L96A1
+AS VAL
+Five SeveN
+G36C
+M9
+P226
+P90
+RPK-74M
 SR-2
-Steyr Elite
+Magnum Revolver
+SKS
+MAC-10
+AUG A3
+HK416
+Makarov
+FAMAS F1
+JNG-90
+CheyTac M200
+MK-11
+QBZ-95
+MP5K
+QBU-88
+Dragunov SVD
 TMP
-Tumor Growth
-UMP-40
+
+--: Ammo :--
+
+Twin 9x19mm Parabellum
+FN 5.7x28mm
+.30-30 Winchester
+.308 Winchester
+.44 Magnum
+.45 ACP
+.50 Action Express
+12 Gauge
+4.6x30mm
+5.56x45mm NATO
+7.62x39mm
+7.92x57mm Mauser
+8 Gauge
+9x19mm Parabellum
+9x39mm
+FN 5.7x28mm Pistol
+.50 BMG
+.40 S&W
+9x19mm Parabellum Rifle
+7.62x51mm NATO
+7.62x39mm M43
+9x21mm Gyurza
+.40 S&W Rifle
+.357 Magnum
+.45 ACP Rifle
+
+--: Other :--
+
+Wrench
+Laptop
+Mask
+NV Goggles
 Balaclava
 Ballistics Helmet
 Ballistics Vest
 Bandage
-Defibrillator
-Fire axe
-Hammer
-Laptop (Drone)
-Mask
 Medkit
-NV Goggles
+Hammer
 Pickaxe
-Wrench
-Akimbo M9 (GAMEPASS)
-Barrett M82A3 (GAMEPASS)
-Classic Pistol (GAMEPASS)
-Cowboy Gun (GAMEPASS)
-Double Barrel (GAMEPASS)
-Fail (GAMEPASS)
-FN FAL (GAMEPASS)
-Groza-4 (GAMEPASS)
-Ithaca 37 Stakeout (GAMEPASS)
-KRISS Vector (GAMEPASS)
-Luger P08 (GAMEPASS)
-M1921 Thompson (GAMEPASS)
-PP2000 (GAMEPASS)
-Riot Shield (GAMEPASS)
-SCAR-H (GAMEPASS)
-Shovel (GAMEPASS) 
-SPAS-12 (GAMEPASS)
-UNICA 6 (GAMEPASS)
-UTS-15 (GAMEPASS)
-Walther WA2000 (GAMEPASS)`.split("\n")
+Fire axe
+Defibrillator
+
+--: Gamepass :--
+
+Groza-4
+Classic Pistol
+Barrett M82A3
+Riot Shield
+PP2000
+Cowboy Gun
+Double Barrel
+SPAS-12
+UNICA 6
+Akimbo M9
+Luger P08
+Ithaca 37 Stakeout
+KRISS Vector
+Shovel
+Fail
+SCAR-H
+UTS-15
+FN FAL
+M1921 Thompson
+Walther WA2000`.split("\n")
 
 const itemtemplate = document.querySelector(".dumpster .item")
 const armortemplate = document.querySelector(".dumpster .armor")
@@ -118,22 +159,25 @@ const attachtemplate = itemtemplate.querySelector(".attach")
 
 const armorselect = armortemplate.querySelector(".armorselect")
 
-for (let i = 0;i < Weapons.length;i++) {
+const addoptioninselect = (sel,name) => {
     const elmnt = document.createElement("option")
-    elmnt.innerHTML = Weapons[i]
-    itemselect.appendChild(elmnt)
+    elmnt.innerHTML = name
+    sel.appendChild(elmnt)
+    if (name.includes("--:")||(name == "")) {
+        elmnt.setAttribute("disabled")
+    }
+}
+
+for (let i = 0;i < Weapons.length;i++) {
+    addoptioninselect(itemselect, Weapons[i])
 }
 
 for (let i = 0;i < Attachments.length;i++) {
-    const elmnt = document.createElement("option")
-    elmnt.innerHTML = Attachments[i]
-    attachselect.appendChild(elmnt)
+    addoptioninselect(attachselect, Attachments[i])
 }
 
 for (let i = 0;i < Armor.length;i++) {
-    const elmnt = document.createElement("option")
-    elmnt.innerHTML = Armor[i]
-    armorselect.appendChild(elmnt)
+    addoptioninselect(armorselect, Armor[i])
 }
 
 const addweapons = document.querySelector(".addweapons")
